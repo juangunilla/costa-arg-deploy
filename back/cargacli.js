e0f82e6fc3b80a7fe812203765f8b2c130ea699f
Juan Manuel Gunilla <68487401+juangunilla@users.noreply.github.com>
2022-06-06 16:40:38 -0300

    first commit

---
+++ back/cargacli.js
@@ -0,0 +1,35 @@
+  var datos = document.getElementById("forms")
+   document.addEventListener("submit", async e=>{
+       if(e.target === datos ){
+           e.preventDefault();
+           try{
+               let options ={
+                   method:"POST",
+                   headers:{
+                       "content-type": "application/json;charset=utf-8",
+                       "api_key": "costa-arg-0306"
+                   },
+                    data:JSON.stringify({
+                       Ciudad:e.target.ciudad.value,
+                       img:e.target.Img.value,
+                       Descripcion:e.target.descripcion.value,
+                       Humedad:e.target.humedad.value,
+                       Viento:e.target.viento.value,
+                       Direccion:e.target.direccion.value,
+                       tMax:e.target.tmax.value,
+                       tMin:e.target.tmin.value,
+                       inUv:e.target.inUv.value  
+                   })
+                
+               },
+               
+               res=await axios("http://localhost:8080/api/climas",options),
+               json= await res.data();
+               alert(data);
+
+           }catch(err){
+
+           }
+           alert("Enviado");
+       }
+   });
